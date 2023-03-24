23ea73d48476b9d33ed487362774ab9fd990a5af
Ahsan <ahsansatti402@gmail.com>
2023-03-24 11:57:12 +0500

    complete ui

---
+++ src/pages/[meetupId]/index.js
@@ -0,0 +1,9 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails(){
+return(
+<MeetupDetail image="https://img.freepik.com/free-photo/male-coworkers-office-with-lunch_273609-6073.jpg?size=626&ext=jpg&uid=R69842242&semt=ais" title="First Meetup" address="IT Park G-13, Islamabad" description="This is a first meetup"/>
+)
+}
+
+export default MeetupDetails;
